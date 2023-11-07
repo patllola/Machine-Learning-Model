74d34f2644e6ce0d5c3c377e79de975659629fd6
Sandeep  Reddy Patllola <53807665+patllola@users.noreply.github.com>
2023-11-07 15:48:59 -0600

    Update logistic.py

--- logistic.py
+++ logistic.py
@@ -8,38 +8,29 @@ import time  # Import the time module for timing and sleeping
 import matplotlib.pyplot as plt  # Import matplotlib for plotting
 
 def perform_inference(image_paths, input_images, model):
-    # Initialize an empty dictionary to store the predictions with image paths as keys
     predictions = {}
 
-    # Initialize empty lists to store the metrics
     timestamps = []
     cpu_percentages = []
     memory_usages = []
     runtimes = []
 
-    # Standardize feature scaling (optional but often improves performance)
     scaler = StandardScaler()
     input_images = scaler.fit_transform(input_images)
 
-    # Perform inference on each input image
     for image_path, image in zip(image_paths, input_images):
-        # Reshape the image as needed (e.g., if it's a single feature vector)
         image = image.reshape(1, -1)
 
-        # Measure system utilization before inference
         start_time = time.time()
         initial_cpu_usage = psutil.cpu_percent()
         initial_memory_usage = psutil.virtual_memory().percent
 
-        # Make predictions using the loaded model
         prediction = model.predict(image)
 
-        # Measure system utilization after inference
         end_time = time.time()
         final_cpu_usage = psutil.cpu_percent()
         final_memory_usage = psutil.virtual_memory().percent
 
-        # Calculate runtime, CPU, and memory usage during inference
         runtime = end_time - start_time
         cpu_usage_during_inference = final_cpu_usage - initial_cpu_usage
         memory_usage_during_inference = final_memory_usage - initial_memory_usage
@@ -48,16 +39,14 @@ def perform_inference(image_paths, input_images, model):
         print("CPU Usage during the program is:", cpu_usage_during_inference,"KB")
         print("Memory Usage during the program is:", memory_usage_during_inference/1024,"KB")
 
-        # Store the prediction in the dictionary
         predictions[image_path] = prediction[0]
 
-        # Append metrics to lists
         timestamps.append(start_time)
         cpu_percentages.append(cpu_usage_during_inference)
         memory_usages.append(memory_usage_during_inference)
         runtimes.append(runtime)
 
-    # Plot the metrics as graphs
+    # Ploting the graphs
     plt.figure(figsize=(12, 8))
     plt.subplot(2, 2, 1)
     plt.plot(timestamps, cpu_percentages)
@@ -86,10 +75,8 @@ if __name__ == "__main__":
     model_filename = "/home/scooby/Desktop/modelfile/logisticregression.pkl"
     loaded_model = joblib.load(model_filename)
 
-    # Define the path to the folder containing test images
     test_folder_path = '/home/scooby/Desktop/Crack Detection/Input_data/Test/'
 
-    # Initialize empty lists to store image paths and data
     image_paths = []
     input_images = []
     for image_file in os.listdir(test_folder_path):
